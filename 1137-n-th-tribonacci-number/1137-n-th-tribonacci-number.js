c7292c7dcaa9a6c80b2a53f98f7d1ca93549c273
shubham kumar <140818738+shubham8603@users.noreply.github.com>
2024-04-15 18:51:13 +0530

    Time: 44 ms (89.42%), Space: 48.4 MB (75.98%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ const cache=[0,1,1];
+var tribonacci = function(n) {
+    
+    if (cache[n] === undefined) {
+     cache[n]=(tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1));
+        }
+
+    
+    return cache[n];
+    
+};
