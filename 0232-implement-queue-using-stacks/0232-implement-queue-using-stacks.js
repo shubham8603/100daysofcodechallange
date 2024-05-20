425ada2ff3c54894fbf83e53c5cc208d4bd94287
shubham kumar <140818738+shubham8603@users.noreply.github.com>
2024-05-20 06:28:35 +0530

    Time: 45 ms (84.69%), Space: 48.8 MB (53.70%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -0,0 +1,41 @@
+class MyQueue {
+    constructor() {
+        this.stack1 = [];
+        this.stack2 = [];
+    }
+
+    push(x) {
+        this.stack1.push(x);
+    }
+
+    pop() {
+        if (this.stack2.length === 0) {
+            while (this.stack1.length > 0) {
+                this.stack2.push(this.stack1.pop());
+            }
+        }
+        return this.stack2.pop();
+    }
+
+    peek() {
+        if (this.stack2.length === 0) {
+            while (this.stack1.length > 0) {
+                this.stack2.push(this.stack1.pop());
+            }
+        }
+        return this.stack2[this.stack2.length - 1];
+    }
+
+    empty() {
+        return this.stack1.length === 0 && this.stack2.length === 0;
+    }
+}
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
