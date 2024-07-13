5d95a612dbcd6c087a0ea6e93d04685f5fc0f5d6
shubham kumar <140818738+shubham8603@users.noreply.github.com>
2024-07-13 15:49:09 +0530

    Time: 97 ms (50.17%), Space: 59.6 MB (7.36%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+    if (root === null) {
+        return new TreeNode(val);
+    }
+    if (val < root.val) {
+        if (root.left === null) {
+            root.left = new TreeNode(val);
+        } else {
+            insertIntoBST(root.left, val);
+        }
+    } else {
+        if (root.right === null) {
+            root.right = new TreeNode(val);
+        } else {
+            insertIntoBST(root.right, val);
+        }
+    }
+    
+    return root;
+};
